74e4699bc250716632fb5346c1b787e91636acef
mubixd69-ux <mubixd69@gmail.com>
2026-09-03 02:04:21 +0300

    Coded the hand
    ok so first I coded a hand(more info abt what I mean by a hand in player.tscn) made the hand follow the direction of the mouse pointer for aiming purposes with a fixed offset and when I make the hand hold a gun the gun automatically aims at the direction of the pointer!

--- player.gd
+++ player.gd
@@ -2,11 +2,19 @@ extends CharacterBody2D
 
 @export var movement_speed : float = 500
 var character_direction : Vector2
+var hand_direction : Vector2
+var hand_offset = 17
+var hand_angle = 0
 
 func _physics_process(delta: float) -> void:
 	character_direction.x = Input.get_axis("left", "right")
 	character_direction.y = Input.get_axis("down", "up")
 	character_direction = character_direction.normalized()
+	hand_direction = (get_global_mouse_position()-$".".position).normalized()
+	$Hand.position.x = hand_direction.x * hand_offset
+	$Hand.position.y = -6 + hand_direction.y * hand_offset
+	hand_angle = rad_to_deg(atan2(hand_direction.y, hand_direction.x))
+	$Hand.rotation_degrees = hand_angle
 	
 	# flip
 	if character_direction.x > 0: %AnimatedSprite2D.flip_h = false
