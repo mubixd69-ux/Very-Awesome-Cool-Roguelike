9437c30e3007b920edb4a4852ece877c8467583f
mubixd69-ux <mubixd69@gmail.com>
2026-09-04 22:20:58 +0300

    Made the bullet be able to deal damage to the enemy

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,12 +1,17 @@
-extends Node2D
+extends Area2D
 
+const SPEED: int = 150
 
-const SPEED: int = 300
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+	
+func _on_area_entered(area: Area2D) -> void:
+	var target = area.get_parent()
+	if target and target.has_method("take_damage"):
+		target.take_damage(25)
+	print(1)	
+	queue_free()
+	
