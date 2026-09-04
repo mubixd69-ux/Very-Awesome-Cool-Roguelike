ca0a1902174c6ca654ef102068a857a4cce16296
mubixd69-ux <mubixd69@gmail.com>
2026-09-04 22:21:30 +0300

    Made the enemy take damage from the bullet and deal damage to the player
    It deals damage to the player if the enemy's hitbox touches the player

--- scripts/groundy.gd
+++ scripts/groundy.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 @export var speed: float = 150.0
+@export var max_health: int = 50
+
+var current_health: int
+
 var player: Node2D = null
 
 func _physics_process(_delta: float) -> void:
@@ -12,4 +16,18 @@ func _physics_process(_delta: float) -> void:
 
 	var direction: Vector2 = (player.global_position - global_position).normalized()
 	velocity = direction * speed
-	move_and_slide()	
+	move_and_slide()
+	
+func take_damage(amount: int) -> void:
+	current_health -= amount
+	
+	
+	if current_health <= 0:
+		die()
+		
+func die() -> void:
+		queue_free()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(25)
