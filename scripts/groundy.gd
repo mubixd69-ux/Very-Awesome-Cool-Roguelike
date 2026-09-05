76156c0365e453d6638bf0eab94023544b718ef5
mubixd69-ux <mubixd69@gmail.com>
2026-09-05 13:07:05 +0300

    Tuned the enemy and fixed the health
    I made the enemy play a hurt animation whenever it got hit  also made the enemy look at the direction of the player (either right or left). Also there was a problem with the enemy's health its supposed to die from 2 bullets but instead it was dying from 1
    I fixed that an made it the intented 2 bullets

--- scripts/groundy.gd
+++ scripts/groundy.gd
@@ -7,27 +7,46 @@ var current_health: int
 
 var player: Node2D = null
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	current_health = max_health
+
 func _physics_process(_delta: float) -> void:
 	if not is_instance_valid(player):
-		# Search the active scene for a node named "Player"
 		player = get_tree().current_scene.find_child("player", true, false)
 		if not is_instance_valid(player):
-			return # Still no player found, wait for next frame
+			return 
 
 	var direction: Vector2 = (player.global_position - global_position).normalized()
 	velocity = direction * speed
 	move_and_slide()
 	
+	if player.global_position.x < global_position.x:
+		animated_sprite.flip_h = true
+	elif player.global_position.x > global_position.x:
+		animated_sprite.flip_h = false
+	
+	if animated_sprite.animation != "hurt":
+		animated_sprite.play("default")
+	
+func _process(delta: float) -> void:
+	if current_health <= 0 and animated_sprite.animation == "default":
+		die()
+	
 func take_damage(amount: int) -> void:
+	animated_sprite.play("hurt")
 	current_health -= amount
 	
-	
-	if current_health <= 0:
-		die()
 		
 func die() -> void:
-		queue_free()
+	animated_sprite.play("hurt") 
+	queue_free()
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
 		body.take_damage(25)
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+		if animated_sprite.animation == "hurt":
+			animated_sprite.play("default")
