3e0330d96534eaaa54c02a89d06dfaa55120e3ad
mubixd69-ux <mubixd69@gmail.com>
2026-09-05 13:07:20 +0300

    Tried to add a health bar but didnt work

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var max_health: int = 100
 var current_health: int
 
+@onready var health_bar: TextureProgressBar = $CanvasLayer/TextureProgressBar
 
 @export var movement_speed : float = 500
 var character_direction : Vector2
@@ -36,10 +37,17 @@ func _physics_process(delta: float) -> void:
 	# Player Health
 func _ready() -> void:
 		current_health = max_health
+		
+		if health_bar:
+			health_bar.max_value = max_health
+			health_bar.value = current_health
 
 func take_damage(amount: int) -> void:
 	current_health -= amount
 	
+	if health_bar:
+		health_bar.value = current_health
+		
 	if current_health <= 0:
 		die()
 		
